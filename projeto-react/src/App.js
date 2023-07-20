2490e1079ff09ebe4b0b1602e77e271cfd6e9a1e
Bianca Ramos Silva <bianca@tentaculo.digital>
2023-07-20 16:38:44 -0300

    primeiro commit

---
+++ projeto-react/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Teste from "./teste";
+
+function App() {
+  return (
+    <div>
+      <Teste></Teste>
+    </div>
+  );
+}
+
+export default App;
---
+++ projeto-react/src/teste.jsx
@@ -0,0 +1,11 @@
+function teste() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Bianca</p>
+      </header>
+    </div>
+  );
+}
+
+export default teste;
